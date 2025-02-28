00f8fbcd7754b0f2a1e1640d528089e047344156
wisher <wisherjls@gmail.com>
2025-02-27 21:33:06 -0600

    add practice examples

--- src/strings.js
+++ src/strings.js
@@ -1,11 +1,23 @@
 const student1 = {
-	name: "Jonathan Sayers",
-	course: "Web Development 1",
+	name: "Sarah",
+	course: "JavaScript",
 	grade: 95,
 };
+const student2 = {
+	name: "John",
+	course: "Python",
+	grade: 88,
+};
+const student3 = {
+	name: "Alice",
+	course: "C++",
+	grade: 92,
+};
 
-function displayStudentInfo(student) {
-	return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%`;
+function displayStudentInfo(name, course, grade) {
+	return `${name} is taking ${course} and expects to earn ${grade}%`;
 }
 
-console.log(displayStudentInfo(student1));
+console.log(displayStudentInfo("Sarah", "JavaScript", 95));
+console.log(displayStudentInfo("John", "Python", 88));
+console.log(displayStudentInfo("Alice", "C++", 92));
