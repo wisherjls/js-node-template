10b5dff77f49624e908377b9b08bac95a8a80704
wisher <wisherjls@gmail.com>
2025-02-27 21:36:15 -0600

    format a username

--- src/functions.js
+++ src/functions.js
@@ -4,3 +4,9 @@ function calculateTotal(order) {
 }
 const order = { price: 10, quantity: 2 };
 console.log(calculateTotal(order)); // Should print: 20
+
+// Function to format a username
+function formatUsername(firstName, lastName) {
+	return `${lastName}, ${firstName}`;
+}
+console.log(formatUsername("John", "Doe")); // Should print: "Doe, John"
