f27aaa4721087a10302bf04e88bba2d8b0db264b
wisher <wisherjls@gmail.com>
2025-03-02 22:44:00 -0600

    Exercise 1: Basic Loop Practice

---
+++ loops-practice.js
@@ -0,0 +1,14 @@
+// A for loop is used when you know the amount of iterations you want to run
+// Using a for loop to count from 1 to 10
+for (let i = 1; i <= 10; i++) {
+	console.log(i); // Output each number to the console
+}
+
+// A while loop is used when you don't know the amount of iterations you want to run
+
+let i = 1;
+while (i <= 10) {
+	// Using a while loop to count from 1 to 10
+	console.log(i); // Output each number to the console
+	i++; // Increment the counter
+}
